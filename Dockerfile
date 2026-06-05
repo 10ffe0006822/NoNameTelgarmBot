4c6b2c4d653d78809c4656e2750780f77579001e
pc <denis88888c@gmail.com>
2026-06-05 16:05:31 +0300

    docker fixes

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
-FROM python:3.11-slim
+FROM python:3.12-slim
 
 RUN pip install aiogram
+RUN pip install aiohttp
 
 COPY main.py /app/main.py
+COPY db.py /app/db.py
 WORKDIR /app
 
 ENV TOKEN=''
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import random
 import string
 import db
+import logging
 
 from aiohttp import web
 
@@ -18,18 +19,20 @@ HOST = os.getenv("HOST", "0.0.0.0")
 PORT = int(os.getenv("PORT", "8888"))
 confirm_code = ""
 
+logger = logging.getLogger(__name__)
+
 
 async def handle(request):
     global bot
     body = await request.json()
     body = body["embeds"][0]
-    msg = f"Новый пост на форуме\n\nТема: {body["title"]}\n\nСодержание: {body['description']}\n\n{html.link("Ссылка", body["url"])}\n\nВремя: {body["timestamp"]}\n\nАвтор: {body["author"]["name"]}"
+    msg = f"Новый пост на форуме\n\nТема: {body['title']}\n\nСодержание: {body['description']}\n\n{html.link('Ссылка', body['url'])}\n\nВремя: {body['timestamp']}\n\nАвтор: {body['author']['name']}"
     try:
         chats = await db.get_chat_id()
         for chat in chats:
             await bot.send_message(chat_id=int(chat), text=msg)
     except Exception as e:
-        print(e)
+        logger.error(e)
     return web.Response(text="OK")
 
 
@@ -52,7 +55,7 @@ async def command_start_handler(message: Message):
         await bot.set_my_commands(commands, scope=BotCommandScopeDefault())
         await bot.set_chat_menu_button(menu_button=MenuButtonCommands())
     except Exception as e:
-        print(e)
+        logger.error(e)
 
 
 @dp.message(Command("help"))
@@ -66,12 +69,12 @@ async def command_help_handler(message: Message):
             await message.answer(
                 f"Команды администратора\n\n/list - вывод всех чатов с активными уведомлениями\n\n/sub - Подписать чат на уведомления\n\n/unsub - отписать чат от уведомлений")
         except Exception as e:
-            print(e)
+            logger.error(e)
     else:
         try:
             await message.answer("Просто отправьте всё что хотите", reply_markup=builder.as_markup())
         except Exception as e:
-            print(e)
+            logger.error(e)
 
 
 @dp.message(Command("confirm"))
@@ -82,24 +85,24 @@ async def command_confirm_handler(message: Message):
             try:
                 await message.answer("Услышал тебя родной!")
             except Exception as e:
-                print(e)
+                logger.error(e)
         elif message.text.split(' ')[1] == confirm_code:
             try:
                 await db.add_admin_id(message.from_user.id)
                 await message.answer("Успех! Вы администратор")
             except Exception as e:
-                print(e)
+                logger.error(e)
         else:
             try:
                 await message.answer("Ошибка! Код не верный")
             except Exception as e:
-                print(e)
+                logger.error(e)
         confirm_code = None
     else:
         try:
             await message.answer("Код не сгенерирован")
         except Exception as e:
-            print(e)
+            logger.error(e)
 
 
 @dp.callback_query(F.data == "I-am-admin")
@@ -109,10 +112,10 @@ async def make_admin(callback: CallbackQuery):
         await callback.answer()
         await callback.message.delete()
         confirm_code = ''.join(random.choices(string.digits, k=16))
-        print(confirm_code)
+        logger.info(f"Confirm code: {confirm_code}")
         await callback.message.answer(f"Для подтверждения введите код из консоли бота\n{html.code('/confirm код')}")
     except Exception as e:
-        print(e)
+        logger.error(e)
 
 
 @dp.message(Command("sub"))
@@ -123,7 +126,7 @@ async def command_sub(message: Message):
             await db.add_chat_id(message.chat.id, message.chat.full_name)
             await message.answer(f"Чат {message.chat.full_name} подписан на уведомления")
         except Exception as e:
-            print(e)
+            logger.error(e)
 
 
 @dp.message(Command("unsub"))
@@ -134,7 +137,7 @@ async def command_sub(message: Message):
             await db.remove_chat_id(message.chat.id)
             await message.answer(f"Чат {message.chat.full_name} отписан от уведомлений")
         except Exception as e:
-            print(e)
+            logger.error(e)
 
 
 @dp.message(Command("list"))
@@ -147,7 +150,7 @@ async def command_sub(message: Message):
                 msg = msg + f"{item}, "
             await message.answer(f"Чаты подписанные на уведомления: {msg}")
         except Exception as e:
-            print(e)
+            logger.error(e)
 
 
 @dp.message()
